be3df2b7ed17b0d558fb98c288bbe59374971c73
raphael <raphaeljr28@gmail.com>
2014-11-18 17:42:04 +0000

    Add files Changed Files

---
+++ minium-pupino-webapp/src/main/webapp/scripts/controllers/launch.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var LaunchController = function($scope, $modalInstance, $state, $stateParams, launcherService) {
+    
+    launcherService.launch($scope.launchParams)
+        .success(function(data) {
+            var steps = jsonPath.eval(data, "$..steps[@.result.status=='passed']");
+            editor.getSession().setAnnotations(_(steps).map(function(step) {
+                return {
+                    row: step.line,
+                    text: "You have a problem here.",
+                    type: "error"
+                };
+            }));
+        });
+};

1c5d2d50a59d22f0c2874e4fa34bc3ac8a08aaf9
mario.lameiras <mario.lameiras@vilt-group.com>
2014-11-26 18:13:03 +0000

    Removed deployed folder added script to build docker image

--- minium-pupino-webapp/deploy/heroku/src/main/java/minium/pupino/security/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring Security configuration.
- */
-package minium.pupino.security;
--- minium-pupino-webapp/deploy/openshift/src/main/webapp/scripts/project/service_project.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-pupinoApp.factory('Project', function ($resource) {
-        return $resource('app/rest/projects/:id', {}, {
-            'query': { method: 'GET', isArray: true},
-            'get': { method: 'GET'}
-        });
-    });

d27cf109ad51c094e725c17ef7b87959e333fb79
mario.lameiras <mario.lameiras@vilt-group.com>
2014-11-26 18:28:16 +0000

    Added script to build and run docker

---
+++ run_docker.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+echo $DIR
+sudo docker run -d -v $DIR/minium-pupino-docker/logs:/opt/pupino/logs -d -v $DIR/minium-pupino-webapp/target:/opt/pupino/bin -p 8080:8080 -p 8081:8081 mariolameiras/pupino pupino
+

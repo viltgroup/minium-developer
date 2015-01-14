ff716ed8d59016f3bd50e8ac688ca62ff976e0e3
raphael <raphaeljr28@gmail.com>
2015-01-14 11:57:26 +0000

    Minor Changes

--- minium-pupino-core/src/main/java/minium/pupino/jenkins/ReporterParser.java
+++ minium-pupino-core/src/main/java/minium/pupino/jenkins/ReporterParser.java
@@ -2,6 +2,7 @@ package minium.pupino.jenkins;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,14 +28,10 @@ public class ReporterParser {
 	 * @throws FileNotFoundException
 	 */
 	public List<Feature> parseJsonResult(String results) throws JsonSyntaxException, JsonIOException, FileNotFoundException {
-		Map<String, Feature> features = new HashMap<String, Feature>();
+		List<Feature> features = new ArrayList<Feature>();
 		Feature[] features1 = new Gson().fromJson(results, Feature[].class);
-		for (Feature f : features1) {
-			features.put(f.getUri(), f);
-		}
-		
-		List<Feature> list = new ArrayList<Feature>(features.values());
-		return list;
+		features.addAll(Arrays.asList(features1));
+		return features;
 	}
 	
 	public Map<String, Feature> parseJsonResultSet(String results) throws JsonSyntaxException, JsonIOException, FileNotFoundException {

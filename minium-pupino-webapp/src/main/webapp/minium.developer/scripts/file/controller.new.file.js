edac356c5445641220cd4d91f23ad157e6632564
raphael <raphaeljr28@gmail.com>
2015-02-03 15:56:25 +0000

    Rename Files

---
+++ minium-pupino-webapp/src/main/webapp/minium.developer/scripts/file/controller.new.file.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var NewFileController = function($scope, $modalInstance, $state, $controller, $stateParams, $log, $location, FS, FileFactory, FormatService, launcherService, MiniumEditor) {
+
+    //extends the fileController
+    $controller('FileController', {
+        $scope: $scope
+    });
+    $scope.fileName = "";
+
+    var parentScope = MiniumEditor.getScope();
+
+    console.log(parentScope)
+
+    
+    if (parentScope.selectedNode !== "" && parentScope.selectedNode.type !== "FILE") {
+        $scope.fs.current = parentScope.selectedNode;
+        // $scope.loadChildren(parentScope.selectedNode);
+    }else{
+        $scope.asyncLoad($scope.fs.current);
+    }
+    
+    $scope.createFile = function(fileName, path) {
+        var fs = $scope.fs.current.relativeUri || "";
+        FileFactory.create(fs + fileName).success(function() {
+            //$scope.asyncLoad($scope.fs.current);
+            toastr.success("Created file " + $scope.fileName);
+            $scope.fileName = "";
+            //reload the children where the file was created
+            parentScope.loadChildren($scope.fs.current);
+            $scope.$close(true);
+        }).error(function(data) {
+            toastr.error("Error " + data);
+        });
+    }
+
+    $scope.ok = function() {
+        $scope.$close(true);
+    };
+
+    $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+        $scope.$dismiss();
+    };
+
+};
---
+++ minium-pupino-webapp/src/main/webapp/minium.manager/scripts/feature/controller.feature.js
@@ -0,0 +1,168 @@
+'use strict';
+
+miniumManager.controller('FeatureController', function($scope, $stateParams, $sce, $state,$modal, resolvedProject, Project, JenkinsProvider, BuildProject, BuildFacade) {
+
+    $state.go('global.feature.scenarios');
+    $scope.project = resolvedProject;
+    $scope.buildId = $stateParams.buildId;
+    var featureURI = $stateParams.featureURI;
+
+    //init variables
+    $scope.background = {};
+    $scope.scenarios = [];
+    $scope.faillingScenarios = [];
+
+    var getFeatureDetails = function() {
+        BuildProject.findByFeature(
+            $scope.buildId,
+            featureURI
+        ).success(function(data) {
+            // console.log(data);
+            //refactor
+            //better get only one object in buildFacade
+            //and in the view get the field that we want
+            var buildFacade = new BuildFacade(data);
+
+            $scope.build = buildFacade.build;
+            $scope.feature = buildFacade.feature;
+        
+            $scope.scenarios = buildFacade.scenarios;
+
+            $scope.faillingScenarios = buildFacade.faillingScenarios
+
+            //console.log($scope.faillingScenarios);
+            processData();
+
+            console.log(buildFacade)
+
+        }).error(function(data) {
+            toastr.error("Error " + data);
+        });
+    }
+
+    getFeatureDetails();
+
+    var colorArray = ['green', 'red', 'yellow', 'blue'];
+    $scope.colorFunction = function() {
+        return function(d, i) {
+            return colorArray[i];
+        };
+    }
+    var processData = function() {
+
+        $scope.exampleData1 = [{
+            key: "Steps",
+            values: [
+                ["Success", $scope.feature.numberOfPasses],
+                ["Failling", $scope.feature.numberOfFailures],
+                ["Skipped", $scope.feature.numberOfSkipped],
+                ["Undifined", $scope.feature.numberOfUndefined]
+            ]
+        }];
+
+        $scope.exampleData = [{
+            key: "X",
+            y: 0
+        }, {
+            key: "Y",
+            y: 0
+        }, {
+            key: "Skipped",
+            y: 0
+        }, {
+            key: "X",
+            y: 0
+        }, {
+            key: "Passing",
+            y: $scope.feature.numberOfPasses
+        }, {
+            key: "X",
+            y: 0
+        }, {
+            key: "Failling",
+            y: $scope.feature.numberOfFailures
+        }];
+    }
+
+
+    $scope.xFunction = function() {
+        return function(d) {
+            return d.key;
+        };
+    }
+    $scope.yFunction = function() {
+        return function(d) {
+            return d.y;
+        };
+    }
+
+    /*
+    Used as helper in view
+     */
+
+    $scope.isPassed = function(value) {
+        return (value === "PASSED");
+    }
+
+    $scope.isFailOrSkipped = function(value) {
+        return (value === "FAILED" || value === "SKIPPED");
+    }
+
+    /*
+    Used to render html comming from a json var
+     */
+    $scope.renderHtml = function(html_code) {
+        return $sce.trustAsHtml(html_code);
+    };
+
+
+    $scope.checkStatus = function(status, prefix) {
+        var result = prefix;
+        result.concat("sdsd")
+        switch (status) {
+            case "PASSED":
+                result += 'success';
+                break;
+            case "FAILED":
+                result += 'danger';
+                break;
+            case "SKIPPED":
+                result += 'warning';
+                break;
+            default: //do nothing
+                result += 'info';
+        }
+
+        return result;
+    }
+
+    $scope.openModalImage = function(imageSrc, imageDescription) {
+        $modal.open({
+            templateUrl: "/minium.manager/views/feature/modals/modalImage.html",
+            resolve: {
+                imageSrcToUse: function() {
+                    return imageSrc;
+                },
+                imageDescriptionToUse: function() {
+                    return imageDescription;
+                }
+            },
+            size: 'lg',                         //size of the modal
+            windowClass: 'modal-with-bg-color', //used to set the CSS class of the modal
+            controller: [
+                "$scope", "imageSrcToUse", "imageDescriptionToUse",
+                function($scope, imageSrcToUse, imageDescriptionToUse) {
+                    $scope.ImageSrc = imageSrcToUse;
+                    $scope.width = 1000;
+                    $scope.height = 1000;
+                    return $scope.ImageDescription = imageDescriptionToUse;
+                }
+            ]
+        });
+    };
+
+    $scope.getImgScr = function  (mime_type,data) {
+        return "data:"+ mime_type+ ";base64,"+ data;
+    }
+
+});

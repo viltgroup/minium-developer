25ff0f2bdd379f6b09adfe54021afb9a7e4dd024
raphael.rodrigues <raphael.rodrigues@vilt-group.com>
2015-03-04 10:27:11 +0000

    Added FileDTO

---
+++ minium-tools-fs/src/main/java/minium/tools/fs/domain/FileDTO.java
@@ -0,0 +1,34 @@
+package minium.tools.fs.domain;
+
+public class FileDTO {
+
+	public String oldName;
+	public String newName;
+	
+	
+	public FileDTO(String oldName, String newName) {
+		super();
+		this.oldName = oldName;
+		this.newName = newName;
+	}
+
+	public FileDTO() {
+		super();
+	}
+	
+	public String getOldName() {
+		return oldName;
+	}
+	public void setOldName(String oldName) {
+		this.oldName = oldName;
+	}
+	public String getNewName() {
+		return newName;
+	}
+	public void setNewName(String newName) {
+		this.newName = newName;
+	}
+	
+	
+	
+}

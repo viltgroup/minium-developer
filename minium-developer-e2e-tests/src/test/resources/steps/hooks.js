cb073cd1d5aa28d61166aa41dfc9de3f09ffce72
Rui Figueira <rui.figueira@gmail.com>
2015-07-28 18:35:36 +0100

    e2e tests improved

--- minium-developer-e2e-tests/src/test/resources/steps/hooks.js
+++ minium-developer-e2e-tests/src/test/resources/steps/hooks.js
@@ -1,5 +1,37 @@
-var interpolator = require("utils/interpolator");
+var timeUnits = require("minium/timeunits"),
+    interpolator = require("utils/interpolator");
 
 Before(function () {
   interpolator.resetDefaults();
+});
+
+World(function () {
+  var stableForClickListener = new JavaAdapter(Packages.minium.actions.internal.DefaultInteractionListener, {
+    onBeforeEvent : function (event) {
+      if (event.getInteraction() instanceof Packages.minium.web.internal.actions.ClickInteraction) {
+        var elem = event.getSource().first();
+        var getPosition = function () {
+          return elem.apply(function () {
+            return JSON.stringify($(this).position());
+          });
+        };
+        
+        // ensure stable
+        var prevPos, pos = getPosition();
+        do {
+          elem.waitTime(50, timeUnits.MILLISECONDS);
+          prevPos = pos;
+          pos = getPosition();
+        } while(!_.isEqual(pos, prevPos));
+      }
+    }
+  });
+  browser.configure()
+    .waitingPreset("fast").timeout(2, timeUnits.SECONDS).done()
+    .waitingPreset("very-slow").timeout(30, timeUnits.SECONDS).interval(1, timeUnits.SECONDS).done()
+    .interactionListeners()
+      .clear()
+      .add(minium.interactionListeners.onUnhandledAlert().accept().thenRetry())
+      .add(minium.interactionListeners.onStaleElementReference().thenRetry())
+      .add(stableForClickListener);
 });

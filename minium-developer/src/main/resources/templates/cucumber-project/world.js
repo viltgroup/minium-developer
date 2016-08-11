d7efd380962a39a7221f37ebfd973af23d6a622b
Jose Morgado <jose.morgado@vilt-group.com>
2016-08-11 09:33:36 +0100

    Adjust default world.js configuration #56964

--- minium-developer/src/main/resources/templates/cucumber-project/world.js
+++ minium-developer/src/main/resources/templates/cucumber-project/world.js
@@ -3,18 +3,18 @@ var timeUnits = require("minium/timeunits");
 World(function () {
   browser.configure()
     .defaultTimeout(5, timeUnits.SECONDS)
-    .defaultInterval(1, timeUnits.SECONDS)
+    .defaultInterval(500, timeUnits.MILLISECONDS)
     // Waiting presets
     // for more details, see: http://minium.vilt.io/docs/minium-training/minium-core-js/#waiting-interactions-and-presets
     .waitingPreset("slow")
       .timeout(30, timeUnits.SECONDS)
-      .interval(5, timeUnits.SECONDS)
+      .interval(1, timeUnits.SECONDS)
     .done()
     // Interaction Listeners
     // for more details, see: http://minium.vilt.io/docs/minium-training/minium-core-js/#interaction-listeners
     .interactionListeners()
       .add(minium.interactionListeners.onStaleElementReference().thenRetry())
-      .add(minium.interactionListeners.onUnhandledAlert().accept())
+      //.add(minium.interactionListeners.onUnhandledAlert().accept())
       //.add(minium.interactionListeners.ensureUnexistence($(".loading")).withWaitingPreset("slow"))
       //.add(minium.interactionListeners.onTimeout().when($(".loading")).waitForUnexistence($(".loading")).withWaitingPreset("slow").thenRetry())
     .done();
